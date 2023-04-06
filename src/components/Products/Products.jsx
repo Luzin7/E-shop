b197bce26c9ccae0cb1c89f86ab4d311328be4bb
Luzin7 <lvictor.dutra@gmail.com>
2023-04-06 15:13:29 -0300

    feat(Products.jsx): adiciona componente Products.jsx

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Products() {
+
+	// useEffect(() => {
+	// 	const fetchApi = async () => {
+	// 		const url = 'https://api.mercadolibre.com/sites/MLB/search?q=celular';
+	// 		const response = await fetch(url);
+	// 		const objJson = await response.json();
+	// 		console.log(objJson.results);
+	// 	};
+	// 	fetchApi();
+	// }, []);
+	return (
+		<div>Products</div>
+	);
+}
