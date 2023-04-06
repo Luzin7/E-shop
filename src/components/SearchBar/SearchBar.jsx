5bfa85d2aa6662b25ff9861cae9bb23cde7226d6
Luzin7 <lvictor.dutra@gmail.com>
2023-04-06 15:15:19 -0300

    feat(SearchBar.jsx): otimiza pesquisa
    ainda em construção, mas foi adicionado uma otimização na pesquisa para evitar muitas requisições em pouco tempo.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { InputBar, ButtonSearch, SearchBarWrapper } from './SearchBar.style';
+import getResultsByQuery from '../../services/api';
 
 export default function SearchBar() {
 	const [searchInput, setSearchInput] = useState('');
+	const delayToCallApi = useRef(null);
 
 	const handleSearchInput = (target) => {
 		setSearchInput(target);
 	};
+
+	useEffect(() => {
+		if (searchInput) {
+			if (delayToCallApi.current) clearInterval(delayToCallApi.current);
+
+			delayToCallApi.current = setTimeout(() => {
+				getResultsByQuery(searchInput);
+				console.log(getResultsByQuery(searchInput));
+			}, 1500);
+			
+			return () => clearInterval(delayToCallApi.current);
+		}
+	}, [searchInput]);
 	return (
 		<SearchBarWrapper>
 			<InputBar
