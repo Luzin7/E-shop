c183be85cd4fe1f8fc4114cd2682a7400ea5778a
Luzin7 <lvictor.dutra@gmail.com>
2023-04-06 23:43:47 -0300

    style(Products.style.jsx): adiciona estilo de fundo padrão

---
+++ src/components/Products/Products.style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import DiscountBG from '../../assets/imgs/Discount-cuate.png';
+
+const EmptySearchBackground = styled.div`
+  background-image: url(${DiscountBG});
+  height: 70vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+export default EmptySearchBackground;
