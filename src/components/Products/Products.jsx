7b64bced9e4b4503c50ed3186b400328f8b724ca
Luzin7 <lvictor.dutra@gmail.com>
2023-04-07 21:44:18 -0300

    feat(Products.jsx): organiza e adiciona infos de produtos

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,14 @@
 import React, { useContext } from 'react';
 import { SearchContext } from '../../contexts/SearchContext';
-import EmptySearchBackground from './Products.style';
+import {
+	EmptySearchBackground,
+	ProductContent,
+	ProductFreeShippingWrapper,
+	ProductImage,
+	ProductWrapper,
+	ProductsWrapper,
+} from './Products.style';
+import { MdLocalShipping } from 'react-icons/md';
 
 export default function Products() {
 	const { search } = useContext(SearchContext);
@@ -8,15 +16,28 @@ export default function Products() {
 	return (
 		<>
 			{search ? (
-				<>
+				<ProductsWrapper className="eita">
 					{search.map((item) => (
-						<div key={item.id}>
-							<h1 key={item.id}>{item.title}</h1>
-							<img src={item.thumbnail} alt={item.title} />
-							<span>R${item.price}</span>
-						</div>
+						<ProductWrapper key={item.id}>
+							<ProductContent>
+								<ProductImage>
+									<img src={item.thumbnail} alt={item.title} />
+									{item.shipping.free_shipping ? (
+										<ProductFreeShippingWrapper>
+											<MdLocalShipping className="free__shipping__icon" />
+											<span className="free__shipping__text">Frete grátis</span>
+										</ProductFreeShippingWrapper>
+									) : null}
+								</ProductImage>
+								<span className="product__original-price">
+									{item.original_price ? `R$${item.original_price}` : null}
+								</span>
+								<span className="product__price">R${item.price}</span>
+								<p key={item.id}>{item.title}</p>
+							</ProductContent>
+						</ProductWrapper>
 					))}
-				</>
+				</ProductsWrapper>
 			) : (
 				<EmptySearchBackground alt="Imagem de storyset no Freepik"></EmptySearchBackground>
 			)}
