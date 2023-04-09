d687e0eb585474906b247d1e73af572c88dba3a0
Luzin7 <lvictor.dutra@gmail.com>
2023-04-09 18:46:01 -0300

    feat(Products.jsx): adiciona tela quando não encontra produto

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { SearchContext } from '../../contexts/SearchContext';
+import { CartContext } from '../../contexts/CartContext';
 import {
 	EmptySearchBackground,
+	EmptySearchResultsBackground,
 	ProductContent,
 	ProductFreeShippingWrapper,
 	ProductImage,
@@ -12,32 +14,50 @@ import { MdLocalShipping } from 'react-icons/md';
 
 export default function Products() {
 	const { search } = useContext(SearchContext);
+	const { addProductToCart } = useContext(CartContext);
 
 	return (
 		<>
 			{search ? (
-				<ProductsWrapper className="eita">
-					{search.map((item) => (
-						<ProductWrapper key={item.id}>
-							<ProductContent>
-								<ProductImage>
-									<img src={item.thumbnail} alt={item.title} />
-									{item.shipping.free_shipping ? (
-										<ProductFreeShippingWrapper>
-											<MdLocalShipping className="free__shipping__icon" />
-											<span className="free__shipping__text">Frete grátis</span>
-										</ProductFreeShippingWrapper>
-									) : null}
-								</ProductImage>
-								<span className="product__original-price">
-									{item.original_price ? `R$${item.original_price}` : null}
-								</span>
-								<span className="product__price">R${item.price}</span>
-								<p key={item.id}>{item.title}</p>
-							</ProductContent>
-						</ProductWrapper>
-					))}
-				</ProductsWrapper>
+				search.length > 0 ? (
+					<ProductsWrapper>
+						{search.map((product) => (
+							<ProductWrapper key={product.id}>
+								<ProductContent>
+									<button
+										type="button"
+										onClick={() => addProductToCart(product.id)}
+									>
+                    Compra
+									</button>
+									<ProductImage>
+										<img src={product.thumbnail} alt={product.title} />
+										{product.shipping.free_shipping ? (
+											<ProductFreeShippingWrapper>
+												<MdLocalShipping className="free__shipping__icon" />
+												<span className="free__shipping__text">
+                          Frete grátis
+												</span>
+											</ProductFreeShippingWrapper>
+										) : null}
+									</ProductImage>
+									<span className="product__original-price">
+										{product.original_price
+											? `R$${product.original_price}`
+											: null}
+									</span>
+									<span className="product__price">R${product.price}</span>
+									<p key={product.id}>{product.title}</p>
+								</ProductContent>
+							</ProductWrapper>
+						))}
+					</ProductsWrapper>
+				) : (
+					<>
+						<EmptySearchResultsBackground alt="Imagem de storyset no Freepik"></EmptySearchResultsBackground>
+						<h1>Desculpe, não conseguimos encontrar o que você procura.</h1>
+					</>
+				)
 			) : (
 				<EmptySearchBackground alt="Imagem de storyset no Freepik"></EmptySearchBackground>
 			)}
