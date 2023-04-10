f1baf5b2afbab54367f1270de34d6641545b0350
Luzin7 <lvictor.dutra@gmail.com>
2023-04-10 01:47:36 -0300

    feat(CartContext.jsx): adiciona mais informações para o carrinho
    adicionando mais informações no carrinho que serão exibidas ao usuário, além de criar um estado para fazer a dinamização da visualização do carrinho pela barra de navegação lateral

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -4,15 +4,16 @@ export const CartContext = createContext();
 
 export const CartContextProvider = ({ children }) => {
 	const [productsCart, setProductsCart] = useState([]);
+	const [isSideNavBarActive, setIsSideNavBarActive] = useState(false);
 
-	const addProductToCart = (id) => {
+	const addProductToCart = (id, img, price) => {
 		const copyProductsCart = [...productsCart];
 
 		const product = copyProductsCart.find((product) => product.id === id);
 
-		if(!product){
-			copyProductsCart.push({id: id, qtd: 1});
-		} else{
+		if (!product) {
+			copyProductsCart.push({ id: id, img: img, price: price, qtd: 1 });
+		} else {
 			product.qtd = product.qtd + 1;
 		}
 
@@ -22,7 +23,9 @@ export const CartContextProvider = ({ children }) => {
 	// const removeProductToCar = (id) => {};
 
 	return (
-		<CartContext.Provider value={{ productsCart, setProductsCart, addProductToCart }}>
+		<CartContext.Provider
+			value={{ productsCart, setProductsCart, addProductToCart, isSideNavBarActive, setIsSideNavBarActive }}
+		>
 			{children}
 		</CartContext.Provider>
 	);
