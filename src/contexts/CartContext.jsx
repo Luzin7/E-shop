1bd74082e4057d2e90dbf7dbd2899bbbd5218b5b
Luzin7 <lvictor.dutra@gmail.com>
2023-04-14 23:15:22 -0300

    feat(CartContext.jsx): adiciona funções para o carrinho
    adicionando função de remoção de produto e limpeza completa do carrinho de compras

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -6,13 +6,13 @@ export const CartContextProvider = ({ children }) => {
 	const [productsCart, setProductsCart] = useState([]);
 	const [isSideNavBarActive, setIsSideNavBarActive] = useState(false);
 
-	const addProductToCart = (id, img, price) => {
+	const addProductToCart = (id, img, price, free_shipping) => {
 		const copyProductsCart = [...productsCart];
 
 		const product = copyProductsCart.find((product) => product.id === id);
 
 		if (!product) {
-			copyProductsCart.push({ id: id, img: img, price: price, qtd: 1 });
+			copyProductsCart.push({ id: id, img: img, price: price, free_shipping: free_shipping, qtd: 1 });
 		} else {
 			product.qtd = product.qtd + 1;
 		}
@@ -20,11 +20,34 @@ export const CartContextProvider = ({ children }) => {
 		setProductsCart(copyProductsCart);
 	};
 
-	// const removeProductToCar = (id) => {};
+	const removeProductToCar = (id) => {
+		const copyProductsCart = [...productsCart];
+		const product = copyProductsCart.find((product) => product.id === id);
+
+		if (product && product.qtd > 1) {
+			product.qtd = product.qtd - 1;
+			setProductsCart(copyProductsCart);
+		} else {
+			const filteredProductsCart = copyProductsCart.filter(
+				(product) => product.id !== id
+			);
+			setProductsCart(filteredProductsCart);
+		}
+	};
+
+	const clearCart = () => setProductsCart([]);
 
 	return (
 		<CartContext.Provider
-			value={{ productsCart, setProductsCart, addProductToCart, isSideNavBarActive, setIsSideNavBarActive }}
+			value={{
+				productsCart,
+				setProductsCart,
+				addProductToCart,
+				removeProductToCar,
+				isSideNavBarActive,
+				setIsSideNavBarActive,
+				clearCart
+			}}
 		>
 			{children}
 		</CartContext.Provider>
