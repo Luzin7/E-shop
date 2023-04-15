214fd8cae2e6759e3edb045539268ba3f4eef525
Luzin7 <lvictor.dutra@gmail.com>
2023-04-15 00:08:15 -0300

    fix(App.jsx): corrige importação do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import Products from './components/Products/Products';
 import { SearchContextProvider } from './contexts/SearchContext';
 import { CartContextProvider } from './contexts/CartContext';
