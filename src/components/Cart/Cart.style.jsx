44504801910f930b1acfa5523505c6b8b703b833
Luzin7 <lvictor.dutra@gmail.com>
2023-04-14 23:04:51 -0300

    style(Cart.style.jsx): adiciona estilo ao carrinho
    adicionando estilo para desktops e mobile

--- src/components/Cart/Cart.style.jsx
+++ src/components/Cart/Cart.style.jsx
@@ -1,70 +1,135 @@
 import styled from 'styled-components';
 
 const SideNavBar = styled.div`
-  background-color: white;
-  width: 21vw;
-  height: 100vh;
-  position: fixed;
-  right: 0%;
-  top: 0;
-  z-index: 999;
-  color: black;
+	background-color: white;
+	width: 15vw;
+	height: 100vh;
+	position: fixed;
+	right: 0%;
+	top: 0;
+	z-index: 999;
+	color: black;
+
+	@media screen and (max-width: 412px) {
+		width: 100vw;
+	}
 `;
 
 const SideNavBarHeader = styled.div`
-  background-color: #ffdeb4;
-  height: 10vh;
-  width: 100%;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-evenly;
-  align-items: center;
+	width: 100%;
+	display: flex;
+	flex-flow: column wrap;
+	justify-content: center;
+	align-items: center;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
-const SideNavBarCloseButton = styled.button`
-  border: none;
-  font-size: 2em;
-  display: flex;
-  justify-content: center;
-  align-items: right;
-  background: none;
-  cursor: pointer;
-  width: 50px;
+const AlertAboutProducts = styled.p`
+	text-align: center;
+	padding: 0 12px;
+	font-size: 12px;
+
+	@media screen and (max-width: 412px) {
+		padding: 0 25px;
+	}
 `;
 
 const SideNavBarProductsWrapper = styled.ul`
-  height: 100vh;
-  overflow-y: scroll;
+	padding: 20px 0;
+	height: 90vh;
+	overflow-y: scroll;
 `;
 
 const Product = styled.li`
-  list-style: none;
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
-  align-items: center;
-text-align: center;
-border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+	list-style: none;
+	display: flex;
+	flex-flow: column wrap;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+	 
+	 &:last-child{
+		margin-bottom: 60px;
+	 }
 `;
 
 const ProductImage = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100px;
-  height: 100px;
-  overflow: hidden;
+	display: flex;
+	align-items: center;
+	width: 100px;
+	height: 100px;
+	overflow: hidden;
 
-  img {
-    width: auto;
-    height: 100%;
-  }
+	img {
+		width: auto;
+		height: 100%;
+	}
+
+	@media screen and (max-width: 412px) {
+		margin-top: 20px;
+	}
+`;
+
+const QuantityOfProduct = styled.div`
+	padding: 7px 0;
+	width: 100px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	@media screen and (max-width: 412px) {
+		margin: 7px 0;
+		width: 200px;
+		.btn-icon {
+			font-size: 28px;
+		}
+	}
+`;
+
+const TotalPriceLabel = styled.span`
+	margin-top: 12px;
+	font-size: 14px;
+	color: black;
+
+	@media screen and (max-width: 412px) {
+		font-size: 18px;
+	}
+`;
+
+const TotalPriceValue = styled.span`
+	font-size: 18px;
+	font-weight: bold;
+	color: #b2a4ff;
+
+	@media screen and (max-width: 412px) {
+		font-size: 24px;
+	}
+`;
+
+const ClearCartBtn = styled.button`
+	margin: 8px 0px 12px 0px;
+	padding: 2px 8px;
+	border-radius: 7px;
+
+	&:hover {
+		background-color: #dbdbdb;
+	}
+
+	@media screen and (max-width: 412px) {
+		padding: 14px 16px;
+	}
 `;
 
 export {
 	SideNavBar,
 	SideNavBarHeader,
-	SideNavBarCloseButton,
 	SideNavBarProductsWrapper,
 	Product,
 	ProductImage,
+	QuantityOfProduct,
+	TotalPriceLabel,
+	TotalPriceValue,
+	AlertAboutProducts,
+	ClearCartBtn,
 };
