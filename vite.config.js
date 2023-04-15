cec499e6c016178623972e7e21aa19f8daa6b242
Luzin7 <lvictor.dutra@gmail.com>
2023-04-15 00:41:11 -0300

    chore(vite.config.js): adicionando url base para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	base: '/E-shop',
 	plugins: [react()],
 	server: {
-		port: 3000
-	}
+		port: 3000,
+	},
 });
